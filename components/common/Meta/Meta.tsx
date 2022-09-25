0813921f12333f551d9502f3bcc3287dc1559936
Jeremy Theintz <jeremy@appogee.ch>
2022-09-26 00:15:54 +0200

    add thumbnail

--- components/common/Meta/Meta.tsx
+++ components/common/Meta/Meta.tsx
@@ -10,8 +10,15 @@ const Meta: FC<IMeta> = ({ title, description, children }) => (
 		<meta name='description' content={description} />
 		<meta property='og:title' content={title} key='title' />
 		<meta property='og:description' content={description} />
+		<meta property='og:image' content='https://create-nextjs-d.app/thumbnail.png' />
+
+		<meta name='twitter:card' content='summary' />
+		<meta name='twitter:site' content='@Swiiny_' />
+		<meta name='twitter:creator	' content='@Swiiny_' />
 		<meta name='twitter:title' content={title} />
 		<meta name='twitter:description' content={description} />
+		<meta name='twitter:image' content='https://create-nextjs-d.app/thumbnail.png' />
+		<meta name='twitter:image:alt' content='Create Nextjs Dapp twitter card' />
 
 		{children}
 	</Head>
