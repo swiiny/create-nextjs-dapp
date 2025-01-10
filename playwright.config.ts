1da5a16beda9d9ccb34954881fc9b3131eae6752
Jeremy Theintz <jeremy@appogee.ch>
2025-01-10 13:39:55 +0100

    test: update playwright config

---
+++ playwright.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+	testDir: './tests',
+	snapshotDir: './tests/snapshots',
+	workers: 2,
+	retries: 2,
+	reporter: [
+		['list', {}],
+		['html', { open: 'on-failure' }]
+	],
+	use: {
+		trace: 'retain-on-failure'
+	},
+	projects: [
+		{
+			name: 'chromium_desktop',
+			testMatch: '**/*.desktop.spec.ts',
+			use: {
+				headless: true,
+				...devices['Desktop Chrome'],
+				viewport: { width: 1280, height: 720 },
+				permissions: ['clipboard-read', 'clipboard-write']
+			}
+		},
+		{
+			name: 'safari_mobile',
+			testMatch: '**/*.mobile.spec.ts',
+			use: { headless: true, ...devices['iPhone 13'], hasTouch: true, isMobile: true }
+		}
+	]
+});
